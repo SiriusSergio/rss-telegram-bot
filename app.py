0d0b39fb2e68c06d81bfed59f51fe7bf0953658d
Sergio Davydiuk <sergio@Sergios-MacBook-Air.local>
2024-06-02 17:14:12 +0300

    temp added

--- app.py
+++ app.py
@@ -11,40 +11,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_weather():
-    # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
-    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-    openmeteo = openmeteo_requests.Client(session=retry_session)
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
 
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-	    "latitude": 55.7426,
-	    "longitude": 37.7778,
-	    "current": ["relative_humidity_2m", "apparent_temperature", "precipitation"]
+    	"latitude": 52.52,
+    	"longitude": 13.41,
+    	"current": "temperature_2m",
+	    "timezone": "GMT",
+	    "forecast_days": 1
     }
     responses = openmeteo.weather_api(url, params=params)
 
-    # Process first location. Add a for-loop for multiple locations or weather models
+# Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
     print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
     print(f"Elevation {response.Elevation()} m asl")
     print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
     print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
-    # Current values. The order of variables needs to be the same as requested.
+# Process daily data. The order of variables needs to be the same as requested.
     current = response.Current()
-    current_relative_humidity_2m = current.Variables(0).Value()
-    current_apparent_temperature = current.Variables(1).Value()
-    current_precipitation = current.Variables(2).Value()
+    current_temperature_2m = current.Variables(0).Value()
 
-    
-    # daily_dataframe = pd.DataFrame(data = daily_data)
-    # return daily_dataframe.to_string(index=None)
-    print(f"Current time {current.Time()}")
-    print(f"Current relative_humidity_2m {current_relative_humidity_2m}")
-    print(f"Current apparent_temperature {current_apparent_temperature}")
-    print(f"Current precipitation {current_precipitation}")
-
+    return current_temperature_2m
 
 def send_message(message):
 
