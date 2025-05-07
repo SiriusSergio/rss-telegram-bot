3d68623285ebda84145c1eb9837de71c81098759
Sergei Davydyuk <162282415+SiriusSergio@users.noreply.github.com>
2025-05-07 16:45:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from telegram.ext import ApplicationBuilder, CallbackQueryHandler, CommandHandle
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
