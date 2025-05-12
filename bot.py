b4bc7cce1b990f36e488543efb10d25223fa0de5
Sergei Davydyuk <dorvardlabeled@gmail.com>
2025-05-12 11:21:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from handlers.start import start_command, button_callback
-from config import TOKEN
 
 def create_bot():
     app = ApplicationBuilder().token(TOKEN).build()
