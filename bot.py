8ab0b7db11e153a78ef78226b58eca957a9c5b57
Sergei Davydyuk <dorvardlabeled@gmail.com>
2025-05-12 11:23:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import os
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from handlers.start import start_command, button_callback
 
+TOKEN = os.getenv("TOKEN")
+
 def create_bot():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start_command))
